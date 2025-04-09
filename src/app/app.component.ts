9adc7a2c014cedc936c8678d0ddc2fc1d089b090
shyam9493 <shyamnandapuneedi123@gmail.com>
2025-04-10 01:57:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
+
+  reloadPage(event: Event) {
+    event.preventDefault();
+    window.location.reload();
+  }
 }
